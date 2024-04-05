66e932482c0e069f9f2501acd5d751a2a4669c17
Emilio Gallo <emilio.gallo17@gmail.com>
2024-04-05 11:46:43 +0200

    add: general structure

---
+++ JSnack_1/js/functions.js
@@ -0,0 +1,18 @@
+function arrayGen (numArray) {
+
+    for ( i = 0; i < numArray; i++) {
+
+        let newArray = [];
+
+        for ( i = 0; i < numArray; i++) {
+
+            newArray.push(Math.floor(Math.random() * 100); )
+    
+        };
+
+        console.log(newArray, "Input Number");
+
+        return newArray;
+    
+    };
+};
---
+++ JSnack_1/js/script.js
@@ -0,0 +1,16 @@
+// TEXT JSnack1
+// Fai inserire un numero, che chiameremo N, all’utente.
+// Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100.
+// Ogni volta che ne crei uno, stampalo a schermo.
+
+const inputNum = parseInt(prompt(`Hello, User! Insert a numer.`));
+
+console.log(inputNum, "Input", typeof inputNum);
+
+for ( i = 0; i < inputNum; i++) {
+
+    let newArray = [];
+
+    console.log(newArray, "Input Number");
+
+};
