f26d68beb9634397c4694cfeced630757e827026
Emilio Gallo <emilio.gallo17@gmail.com>
2024-04-07 17:14:50 +0200

    add: content&script - JSnack3 - html&js

---
+++ JSnack_3/js/functions.js
@@ -0,0 +1,12 @@
+/**
+ * Description: function return true if strings in stringsArray have same length.
+ * @param {object} inputArray array of elements.
+ * @returns {number} number of array elements.
+ */
+function contaElementi(inputArray) {
+
+    let result = inputArray.length;
+
+    return result;
+
+};
---
+++ JSnack_3/js/script.js
@@ -0,0 +1,14 @@
+// TEXT JSnack1
+// Scrivere una funzione contaElementi() che, dato un array in input, restituisca il numero di elementi presenti nell'array
+
+// DATA COLLECTION
+
+// Define constant of given array (inputArray).
+
+const inputArray = ["Sea", "Moon", "Sky", "Stars"];
+
+console.log("inputArray", inputArray, typeof inputArray);
+
+const elementsArrayNum = contaElementi(inputArray);
+
+console.log("Number of elements in the array", elementsArrayNum, typeof elementsArrayNum);
